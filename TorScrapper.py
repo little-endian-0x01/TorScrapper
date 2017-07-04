0c6279a22acce3616dbdfcbb7ee04218ef5e8f6f
ConanKapoor <kapoor.shivam88@gmail.com>
2017-07-05 02:03:01 +0530

    TorScraper file updated

--- TorScrapper.py
+++ TorScrapper.py
@@ -1,20 +1,35 @@
-#Author - Shivam Kapoor (ConanKapoor)
+# Author - Shivam Kapoor (ConanKapoor)
 
-#Importing Essentials
+# Importing Essentials
 from multiprocessing import Pool
+from pyfiglet import Figlet
 import os
 
-#Opening onions directory. To scrape links add the same in onions.txt
+# Opening onions directory. To scrape links add the same in onions.txt
 with open("onions.txt", "r") as onion:
     content = onion.read().splitlines()
 
-#Terminal Process (Only for Gnome at the moment).
-def Execute(url):
-    execute = str('gnome-terminal -e \' python3 Modules/Crawler/main.py ' + url + '\'')
+# Terminal Process to edit Onions.txt using nano.(Only for Gnome at the moment).
+def ExecuteEditor():
+    execute = "nano onions.txt"
     os.system(execute)
 
-#MultiPrcessing Implementation (Limit - 5 processes at a time).
-if __name__ == '__main__':
+# Terminal Process for Crawler (Only for Gnome at the moment).
+def ExecuteCrawler(url):
+    execute = str('gnome-terminal -e \' python3 Modules/Crawler/crawl.py ' + url + '\'')
+    os.system(execute)
+
+# Terminal Process for Scraper (Only for Gnome at the moment).
+def ExecuteScraper(url):
+    execute = str('gnome-terminal -e \' python3 Modules/Scraper/scrape.py ' + url + '\'')
+    os.system(execute)
+
+# Terminal Process for Scraping latest links. (Under Construction)
+def ExecuteDiff():
+    print ("\n------------> Work in progress. The developer is lazy af. <------------\n")
+
+# MultiPrcessing Implementation (Limit - 5 processes at a time).
+def Multiprocessing(task):
     if (os.path.exists("Output")):
         delete = str('rm -r Output')
         os.system(delete)
@@ -22,7 +37,38 @@ if __name__ == '__main__':
     else:
         os.makedirs("Output")
 
-    with Pool(processes = 5) as pool:
+    with Pool(processes=5) as pool:
         for onion in range(0, len(content)):
-            pool.apply(Execute, args=(content[onion],))
+            pool.apply(task, args=(content[onion],))
+
+# Banner for the program.
+def Banner():
+    banner = Figlet(font='slant')
+    print (banner.renderText('TorScraper'))
+    print ("<---------WELCOME TO TORSCRAPER PROGRAM--------->")
+    print ("<---------v1.0 - Author - Conan Kapoor--------->")
+    print ("\n")
+
+# Menu given to users. Eat away!
+def Menu():
+    print ("Please Select the mode of operation:- \n")
+    print ("----> 1) Edit Onions.txt to add links.")
+    print ("----> 2) Crawl given links :].")
+    print ("----> 3) Scrape given links :].")
+    print ("----> 4) Compare latest crawl and scrape the latest links.\n")
+
+if __name__ == '__main__':
+    Banner()
+    Menu()
+    choice = int(input("Enter your choice: "))
+
+    if choice == 1:
+        ExecuteEditor()
+    elif choice == 2:
+        Multiprocessing(ExecuteCrawler)
+    elif choice == 3:
+        Multiprocessing(ExecuteScraper)
+    else:
+        ExecuteDiff()
+
 
